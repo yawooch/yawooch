50996bbdcd49be9139cd1860d8d03de94d0df308
yawooch <yoc0421@naver.com>
2024-06-08 15:45:02 +0900

    예제 스프링 부트 올림

---
+++ projects/springBoot/springboot-developer-2rd-main/chapter10/src/test/java/me/shinsunyoung/springbootdeveloper/controller/TokenApiControllerTest.java
@@ -0,0 +1,135 @@
+package me.shinsunyoung.springbootdeveloper.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.shinsunyoung.springbootdeveloper.config.jwt.JwtFactory;
+import me.shinsunyoung.springbootdeveloper.config.jwt.JwtProperties;
+import me.shinsunyoung.springbootdeveloper.domain.RefreshToken;
+import me.shinsunyoung.springbootdeveloper.domain.User;
+import me.shinsunyoung.springbootdeveloper.dto.CreateAccessTokenRequest;
+import me.shinsunyoung.springbootdeveloper.repository.RefreshTokenRepository;
+import me.shinsunyoung.springbootdeveloper.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TokenApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    JwtProperties jwtProperties;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+
+    User user;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        userRepository.deleteAll();
+    }
+
+    @BeforeEach
+    void setSecurityContext() {
+        userRepository.deleteAll();
+        user = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("test")
+                .build());
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities()));
+    }
+
+    @DisplayName("createNewAccessToken: 새로운 액세스 토큰을 발급한다.")
+    @Test
+    public void createNewAccessToken() throws Exception {
+        // given
+        final String url = "/api/token";
+
+        String refreshToken = createRefreshToken();
+
+        refreshTokenRepository.save(new RefreshToken(user.getId(), refreshToken));
+
+        CreateAccessTokenRequest request = new CreateAccessTokenRequest();
+        request.setRefreshToken(refreshToken);
+        final String requestBody = objectMapper.writeValueAsString(request);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // then
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.accessToken").isNotEmpty());
+    }
+
+    @DisplayName("deleteRefreshToken: 리프레시 토큰을 삭제한다.")
+    @Test
+    public void deleteRefreshToken() throws Exception {
+        // given
+        final String url = "/api/refresh-token";
+
+        String refreshToken = createRefreshToken();
+
+        refreshTokenRepository.save(new RefreshToken(user.getId(), refreshToken));
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(user, refreshToken, user.getAuthorities()));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(delete(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE));
+
+        // then
+        resultActions
+                .andExpect(status().isOk());
+
+        assertThat(refreshTokenRepository.findByRefreshToken(refreshToken)).isEmpty();
+    }
+
+
+    private String createRefreshToken() {
+        return JwtFactory.builder()
+                .claims(Map.of("id", user.getId()))
+                .build()
+                .createToken(jwtProperties);
+    }
+
+}
